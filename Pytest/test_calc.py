d96cd06b8db2419a18756a472b4a83a2050a8b3c
brawenlu <brawenlu@sina.com>
2021-03-29 00:49:41 +0800

    3.29学习allure报告完成

--- Pytest/test_calc.py
+++ Pytest/test_calc.py
@@ -9,6 +9,8 @@
 @time: 2021/3/21 16:53
 @desc:
 '''
+import allure
+
 from Pytest.Caculartor import Calc
 
 # 导入另一个测试方法的函数
@@ -25,6 +27,7 @@ from Pytest.Caculartor import Calc
 
 import pytest,requests,json
 
+@allure.feature("计算机的功能")
 
 class Testcalc():
 
@@ -54,6 +57,7 @@ class Testcalc():
     def teardown_class(self):
         print("结束计算")
 
+    @allure.story("测试加法")
     def test_add(self):
         # calc = Calc()
         result = self.calc.add(1,1)
@@ -91,8 +95,7 @@ class Testcalc():
     # 参数化传输
     # def setup_class(self):
 
-
-
+    @allure.story("测试举例")
     def test_loginfailed(self,username,pwd):
         # print(self.i)
         # print(type(self.i))
@@ -112,33 +115,39 @@ class Testcalc():
     #     username = "Luwenbo11"
     #     pwd = "sangfor123454"
 
-
+    @allure.title("验证测试人帖子topid是否满足")  #只可以在方法上面，@allure.step（‘’）只能放在类或者方法上面，with allure.step（’’）: 可以放在函数里面
 
     @pytest.mark.parametrize('topic_id',[
         "10704","10705","10706","10707"
     ])
+    # @allure.link("https://ceshiren.com/t/topic/10704",name="allure报告")
+    # @allure.testcase(TESTCASELINK, name='测试用例管理平台')
+    @allure.issue("https://ceshiren.com/t/topic/10704",name="bug链接")
+    @allure.story("测试人帖子测试功能")
     # 多个参数需要列表，单个只需要""
-
     def test_ceshiren(self,topic_id):
         # topic_id="10704"
         print(type(topic_id))
         # 使用f可以识别常量里面的变量
-        result=requests.get("https://ceshiren.com/t/topic/{}.json".format(topic_id))
-        # {}format()和f{}都可以用格式化
-
-        print(json.dumps(result.json(),sort_keys=True,indent=4))
-        if "errors" in result.json():
-            # 判断是否存在某个字段
-            print(result.json()["errors"])
-            # print("是")
-        else:pass
-
+        with allure.step("访问【测试人】社区"):
+            result=requests.get("https://ceshiren.com/t/topic/{}.json".format(topic_id))
+            # {}format()和f{}都可以用格式化
+            print(json.dumps(result.json(),sort_keys=True,indent=4))
+        with allure.step("验证结果"):
+            if "errors" in result.json():
+                # 判断是否存在某个字段
+                print(result.json()["errors"])
+                # print("是")
+            else:pass
         # if result.json()["errors"] !="":
         #     print(result.json()["errors"])
         # else:pass
-        respnse_id=result.json()["id"]
-        print(type(respnse_id))
-        assert topic_id==str(respnse_id)
+            respnse_id=result.json()["id"]
+            print(type(respnse_id))
+            assert topic_id==str(respnse_id)
+        allure.attach.file("C:\\Users\\Administrator\\Desktop\\psb.jpg", name="胖嘟",
+                            attachment_type=allure.attachment_type.JPG)
+
 
 
 
